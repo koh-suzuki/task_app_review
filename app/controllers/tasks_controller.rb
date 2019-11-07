402fe79115777cba953115ce12aeaf50d2d7520d
koh-suzuki <k.s_coward_0629@icloud.com>
2019-11-07 06:10:25 +0000

    add new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,20 @@
 class TasksController < ApplicationController
+  
+  def new
+    
+  end
+  
   def index
+    @tasks = Task.all
+  end
+  
+  def edit
+    
+  end
+  
+  def destroy
+  end
+  
+  def show
   end
 end
